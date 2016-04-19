deb1c88d0f6e81bf930b646b9e585b7d7850ab4f
Mads Kristensen <post@madskristensen.net>
2016-04-19 09:12:00 -0700

    Refactorings

--- src/Options.cs
+++ src/Options.cs
@@ -6,31 +6,40 @@ namespace ShowTheShortcut
 {
     public class Options : DialogPage
     {
-        [Category("General")]
+        // General
+        private const string GeneralCategory = "General";
+
+        [Category(GeneralCategory)]
         [DisplayName("Show on shortcut")]
         [Description("Determines if the status bar should show shortcuts for commands that were invoked by a shortcut.")]
         [DefaultValue(false)]
         public bool ShowOnShortcut { get; set; }
 
-        [Category("Status Bar")]
-        [DisplayName("Status bar timeout")]
-        [Description("Specify for how many seconds the to show the shortcut in the status bar. Set to 0 (zero) to never hide.")]
-        [DefaultValue(5)]
-        public int Timeout { get; set; } = 5;
+        // Status Bar
+        private const string StatusBarCategory = "Statur Bar";
 
-        [Category("Status Bar")]
-        [DisplayName("Log to status bar")]
+        [Category(StatusBarCategory)]
+        [DisplayName("Log to Status Bar")]
         [Description("Log all keyboard shortcuts for commands captured to the status bar.")]
         [DefaultValue(true)]
         public bool LogToStatusBar { get; set; } = true;
 
-        [Category("Status Bar")]
+        [Category(StatusBarCategory)]
+        [DisplayName("Timeout")]
+        [Description("Specify for how many seconds the to show the shortcut in the status bar. Set to 0 (zero) to never hide.")]
+        [DefaultValue(5)]
+        public int Timeout { get; set; } = 5;
+
+        [Category(StatusBarCategory)]
         [DisplayName("Show tooltip")]
         [Description("Determines if a tooltiop should be enabled. This can be useful if you want to know more information about the commands.")]
         [DefaultValue(false)]
         public bool ShowTooltip { get; set; }
 
-        [Category("Output Window")]
+        // Output Window
+        private const string OutputWindowCategory = "Output Window";
+
+        [Category(OutputWindowCategory)]
         [DisplayName("Log to Output Window")]
         [Description("Log all keyboard shortcuts for commands captured to the Output Window.")]
         [DefaultValue(true)]
